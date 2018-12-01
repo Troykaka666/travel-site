f91c48b3dc064c4c066942efa0ad0861a5be7a62
Troy <Troywu9527@gmail.com>
2018-12-01 11:46:48 -0500

    adding scrolling affects for feature items and the testimonal

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -14,7 +14,10 @@
 // var jane = new Adult("Jane Smith", "yellow");
 // jane.greet();
 // jane.payTxes();
-
+import $ from "jquery";
 import MobileMenu from "./modules/MobileMenu";
+import RevealOnScroll from "./modules/RevealOnScroll";
 
 var mobileMenu = new MobileMenu();
+new RevealOnScroll($(".feature-item"), "85%");
+new RevealOnScroll($(".testimonial"), "80%");
